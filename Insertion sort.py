876ca3380414dafb4f147287fa31a2e661556cf7
MADAVARAM LAYA <madavaramlaya@gmail.com>
2020-12-17 20:34:15 +0530

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -1,3 +1,4 @@
+# Function used to sort a list using Insertion sort algorithm. 
 def insertionSort(li):
     t=len(li)
     for i in range(1,t,1):
@@ -8,5 +9,9 @@ def insertionSort(li):
             j-=1 
         li[j+1]=key
     return li 
+
+# Taking a list as input.
 s=list(map(int,input().split())) 
+
+# Displaying sorted list.
 print(*insertionSort(s))
